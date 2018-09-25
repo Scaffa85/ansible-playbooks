08dae37915e3d28e72becbfab24911563ef0e980
Craig Simpson <csimpson@csimpson-capgemini.local>
2018-09-25 03:11:31 +0100

    Added '--help' and '-h' flags to ansible-create, as well as the initial steps to start parametirizing the addition of a new role

--- ansible-create.py
+++ ansible-create.py
@@ -1,11 +1,20 @@
-#!/usr/local/bin/python
+#!/usr/local/bin/python3
 import os
+import argparse
 
 # This script sets up ansible playbooks folder structures, as per;
 # http://docs.ansible.com/ansible/latest/user_guide/playbooks_best_practices.html
 
+# Set's up '-h' flag and populates with information.
+parser=argparse.ArgumentParser(
+        description='''This script sets up an ansible playbook folder structure as per the best practices prescribed by RedHat.''')
+parser.add_argument('--role', default='common', help='Enter the name of a new role you would like to initialize' )
+args=parser.parse_args()
+
+# Directory's which exist at the base level of the playbooks directory.
 dir_list = ["group_vars", "host_vars", "library", "module_utils", "filter_plugins",]
 
+# Directory's which are nested inside a role.
 dir_nest = ["roles/common/tasks","roles/common/handlers","roles/common/templates","roles/common/files","roles/common/vars","roles/common/defaults","roles/common/meta","roles/common/library","roles/common/module_utils","roles/common/lookup_plugins"]
 
 
